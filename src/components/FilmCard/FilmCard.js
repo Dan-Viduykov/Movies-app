5ee96c06d41697344f77f5c29cfa303372535186
Danya_Vid <danilviduykov@gmail.com>
2022-01-29 20:32:22 +0500

    add requer data from movies-db

--- src/components/FilmCard/FilmCard.js
+++ src/components/FilmCard/FilmCard.js
@@ -1,21 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './FilmCard.css';
 
-export default function FilmCard() {
+export default function FilmCard({ data }) {
+  const { title, release_date: releaseDate, overview, poster_path: posterPath } = data;
+
   return (
     <div className="film-card">
       <div className="film-card__image">
-        <img src="https://clrphotos.com/wp-content/uploads/2019/02/placeholder_image1-9.png" alt="film name poster" />
+        <img src={`https://image.tmdb.org/t/p/w500/${posterPath}`} alt={`${title} poster`} />
       </div>
       <div className="film-card__content">
-        <h5 className="film-card__title">Film Name</h5>
-        <p className="film-card__date">03.03.2000</p>
+        <h5 className="film-card__title">{title}</h5>
+        <p className="film-card__date">{releaseDate}</p>
         <ul className="film-card__genres">
           <li>Action</li>
           <li>Drama</li>
         </ul>
-        <p className="film-card__overview">lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
+        <p className="film-card__overview">{overview}</p>
       </div>
     </div>
   );
 }
+
+FilmCard.defaultProps = {
+  data: {
+    title: 'no title',
+    release_date: 'no date',
+    overview: 'no description',
+    poster_path:
+      'https://yandex.ru/images/search?from=tabbar&text=placeholder&pos=0&img_url=https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FCpXpQ22WAAAzv-Y.jpg&rpt=simage',
+  },
+};
+
+FilmCard.propTypes = {
+  data: PropTypes.shape({
+    title: PropTypes.string,
+    release_date: PropTypes.string,
+    overview: PropTypes.string,
+    poster_path: PropTypes.string,
+  }),
+};
--- src/components/FilmsList/FilmsList.js
+++ src/components/FilmsList/FilmsList.js
@@ -1,28 +1,45 @@
-import React from 'react';
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
 import FilmCard from '../FilmCard';
+import MoviesService from '../../services/MoviesService';
 import './FilmsList.css';
 
-export default function FilmsList() {
-  return (
-    <ul className="films-list">
-      <li className="films-list__item">
-        <FilmCard />
+export default class FilmsList extends Component {
+  moviesService = new MoviesService();
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      films: [],
+    };
+  }
+
+  componentDidMount() {
+    this.moviesService
+      .searchMovies()
+      .then((response) => response.results)
+      .then((data) => {
+        const filmsList = data.map((film) => this.createCard(film));
+
+        this.setState({
+          films: filmsList,
+        });
+      });
+  }
+
+  createCard(film) {
+    const { id, ...data } = film;
+
+    return (
+      <li className="films-list__item" key={id}>
+        <FilmCard data={data} />
       </li>
-      <li className="films-list__item">
-        <FilmCard />
-      </li>
-      <li className="films-list__item">
-        <FilmCard />
-      </li>
-      <li className="films-list__item">
-        <FilmCard />
-      </li>
-      <li className="films-list__item">
-        <FilmCard />
-      </li>
-      <li className="films-list__item">
-        <FilmCard />
-      </li>
-    </ul>
-  );
+    );
+  }
+
+  render() {
+    const { films } = this.state;
+
+    return <ul className="films-list">{films}</ul>;
+  }
 }
