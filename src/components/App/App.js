4732c0845fd129e2d9506a9a44223524f166f982
Danya_Vid <danilviduykov@gmail.com>
2022-02-03 10:43:22 +0500

    when the user has no network

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,10 @@ import './App.css';
 import FilmsList from '../FilmsList';
 
 export default function App() {
+  if (!window.navigator.onLine) {
+    console.log('offline');
+  }
+
   return (
     <div className="app">
       <FilmsList />
